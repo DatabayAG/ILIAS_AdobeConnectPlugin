fe9b3d670013640fb084e5b10b9bb8f22d4ee605
mjansenDatabay <mjansen@databay.de>
2015-09-16 08:32:27 +0200

    Added function to uninstall plugin

--- classes/class.ilAdobeConnectPlugin.php
+++ classes/class.ilAdobeConnectPlugin.php
@@ -33,4 +33,68 @@ class ilAdobeConnectPlugin extends ilRepositoryObjectPlugin
 			'icon_' . $a_type . '.svg'
 		);
 	}
+
+	/**
+	 * 
+	 */
+	protected function uninstallCustom()
+	{
+		/**
+		 * @var $ilDB ilDB
+		 */
+		global $ilDB;
+
+		if($ilDB->tableExists('rep_robj_xavc_data'))
+		{
+			$ilDB->dropTable('rep_robj_xavc_data');
+		}
+
+		if($ilDB->tableExists('rep_robj_xavc_settings'))
+		{
+			$ilDB->dropTable('rep_robj_xavc_settings');
+		}
+
+		if($ilDB->tableExists('rep_robj_xavc_users'))
+		{
+			$ilDB->dropTable('rep_robj_xavc_users');
+		}
+
+		if($ilDB->tableExists('rep_robj_xavc_members'))
+		{
+			$ilDB->dropTable('rep_robj_xavc_members');
+		}
+
+		if($ilDB->tableExists('rep_robj_xavc_gloperm'))
+		{
+			$ilDB->dropTable('rep_robj_xavc_gloperm');
+		}
+
+		if($ilDB->sequenceExists('rep_robj_xavc_gloperm'))
+		{
+			$ilDB->dropSequence('rep_robj_xavc_gloperm');
+		}
+
+		foreach(array('cb_extended', 'cb_simple') as $settings_tpl)
+		{
+			$ilDB->manipulateF(
+				'DELETE FROM adm_settings_template WHERE type = %s  AND title = %s',
+				array('text', 'text'),
+				array('xavc', $settings_tpl)
+			);
+		}
+
+		foreach(array('il_xavc_admin', 'il_xavc_member') as $tpl)
+		{
+			$obj_ids = ilObject::_getIdsForTitle($tpl, 'rolt');
+			foreach($obj_ids as $obj_id)
+			{
+				$obj = ilObjectFactory::getInstanceByObjId($obj_id, false);
+				if(!($obj instanceof ilObjRoleTemplate))
+				{
+					continue;
+				}
+				$obj->delete();
+			}
+		}
+	}
 }
--- plugin.php
+++ plugin.php
@@ -5,13 +5,13 @@
 $id = 'xavc';
 
 // code version; must be changed for all code changes
-$version = '3.0.0';
+$version = '3.1.0';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = '5.0.0';
-$ilias_max_version = '5.0.999';
+$ilias_max_version = '5.1.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
-$responsible      = 'Nadia Ahmad';
-$responsible_mail = 'nahmad@databay.de';
+$responsible      = 'Nadia Matuschek';
+$responsible_mail = 'nmatuschek@databay.de';
