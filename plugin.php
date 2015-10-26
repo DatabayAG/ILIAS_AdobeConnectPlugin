2adfe6b3d2bc10e2a6b33520b06fe0b1f38a60f1
nahmad <nahmad@databay.de>
2015-10-26 10:02:05 +0100

    Fixed ILIAS Max. version number

--- plugin.php
+++ plugin.php
@@ -9,9 +9,9 @@ $version = "2.0.1";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.3.0";
-$ilias_max_version = "4.4.9";
+$ilias_max_version = "4.4.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "Nadia Ahmad";
-$responsible_mail = "nahmad@databay.de";
+$responsible = "Nadia Matuschek";
+$responsible_mail = "nmatuschek@databay.de";
 ?>
