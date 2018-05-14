d8493da03652fc1710784eb6599267b08f1ff354
mjansen <mjansen@databay.de>
2018-05-14 15:08:01 +0200

    API: Switched API method to retrieve the principal id (report-* methods MUST NOT be used for search purposes, because they point to the (slow and maybe locked) reporting database and may cause performance/latency issues for ILIAS)
    (cherry picked from commit 8c3f799)

--- classes/class.ilAdobeConnectXMLAPI.php
+++ classes/class.ilAdobeConnectXMLAPI.php
@@ -1259,10 +1259,10 @@ class ilAdobeConnectXMLAPI
 			}
 		}
 
-		$ilLog->write('AdobeConnect addUser Request:  '.$url);
+		$ilLog->write('AdobeConnect searchUser Request:  '.$url);
 		if($xml)
 		{
-			$ilLog->write('AdobeConnect addUser Response: ' . $xml->asXML());
+			$ilLog->write('AdobeConnect searchUser Response: ' . $xml->asXML());
 		}
 
 		return false;
