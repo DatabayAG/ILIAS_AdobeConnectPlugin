2262779f88f5cbccddf93bc75466f2722ae152d7
mjansen <mjansen@databay.de>
2019-05-27 16:22:50 +0200

    Gallery: Pagination fixes

--- classes/class.ilAdobeConnectUsersGalleryCollectionProvider.php
+++ classes/class.ilAdobeConnectUsersGalleryCollectionProvider.php
@@ -9,75 +9,72 @@ include_once './Services/User/classes/class.ilUserUtil.php';
  */ 
 class ilAdobeConnectUsersGalleryCollectionProvider extends ilAbstractUsersGalleryCollectionProvider
 {
+	/** @var ilObjAdobeConnect */
+	protected $object;
+
+	/** @var int */
+	protected $numTotalUsers = 0;
+
+	/** @var int */
+	protected $numValidIterations = 0;
+
 	/**
-	 * @var ilParticipants
+	 * ilAdobeConnectUsersGalleryCollectionProvider constructor.
+	 * @param ilObjAdobeConnect $object
 	 */
-	protected $participants;
-	
-	/**
-	 * @var array
-	 */
-	protected $users = array();
-	
-	/**
-	 * @param ilParticipants $participants
-	 */
-	public function __construct(ilParticipants $participants)
+	public function __construct(ilObjAdobeConnect $object)
 	{
-		$this->participants = $participants;
 	}
-	
+
+	/**
+	 * @return array|ilUsersGalleryUserCollection[]
+	 */
 	public function getGroupedCollections()
 	{
 		$groups = [];
-		$admins = $this->participants->getAdmins();
-		$tutors = $this->participants->getTutors();
-		$members = $this->participants->getMembers();
-		
-		$unique_users = array_unique(array_merge($admins, $tutors, $members));
-		
-		foreach([array(  $unique_users, false, '')] as $users)
-		{
-			$group = $this->getPopulatedGroup($this->getUsers($users[0]));
-			$group->setHighlighted($users[1]);
-			$group->setLabel($users[2]);
-			$groups[] = $group;
-		}
-		
+
+		$group = $this->getPopulatedGroup($this->getUsers((int) $this->object->getRefId()));
+		$group->setHighlighted(false);
+		$groups[] = $group;
+
 		return $groups;
 	}
-	
+
 	/**
-	 * @return mixed
+	 * @param int $refId
+	 * @return array
 	 */
-	public function getUsers()
+	public function getUsers(int $refId) : array 
 	{
 		$this->pluginObj = ilPlugin::getPluginObject('Services', 'Repository', 'robj', 'AdobeConnect');
-		
+
 		$this->pluginObj->includeClass('class.ilAdobeConnectRoles.php');
-		$xavcRoles = new ilAdobeConnectRoles($_GET['ref_id']);
-		$members   = $xavcRoles->getUsers();
-		
-		// MEMBERS
-		if(count($members))
-		{
-			foreach($members as $member_id)
-			{
-				if(!($usr_obj = ilObjectFactory::getInstanceByObjId($member_id, false)))
-				{
-					continue;
-				}
-				
-				if(!$usr_obj->getActive())
-				{
-					continue;
-				}
-				
-				$users[$usr_obj->getId()] = $usr_obj;
-				$this->users[$usr_obj->getId()] = true;
-				
+		$xavcRoles = new ilAdobeConnectRoles($refId);
+		$members = $xavcRoles->getUsers();
+
+		$users = [];
+
+		foreach ($members as $member_id) {
+			if (!($usr_obj = ilObjectFactory::getInstanceByObjId($member_id, false))) {
+				continue;
 			}
+
+			if (!$usr_obj->getActive()) {
+				continue;
+			}
+
+			if (null === $this->offset || $this->numValidIterations >= $this->offset) {
+				if (null !== $this->limit && $this->numTotalUsers >= $this->limit) {
+					break;
+				}
+
+				$users[$usr_obj->getId()] = $usr_obj;
+				++$this->numTotalUsers;
+			}
+
+			++$this->numValidIterations;
 		}
+
 		return $users;
 	}
 	
