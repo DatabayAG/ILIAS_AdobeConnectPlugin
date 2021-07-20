456d5374a76cb7fefd345135537e6dd4fa7462da
nmatuschek <nmatuschek@databay.de>
2021-07-20 09:56:52 +0200

    version upgrade

--- plugin.php
+++ plugin.php
@@ -5,12 +5,12 @@
 $id = 'xavc';
 
 // code version; must be changed for all code changes
-$version = '5.1.2';
+$version = '6.0.0';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '5.3.0';
-$ilias_max_version = '5.4.999';
+$ilias_min_version = '6.0';
+$ilias_max_version = '7.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible      = 'Nadia Matuschek';
